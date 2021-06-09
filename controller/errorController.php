6a79fd2a9813d2aaa83c50feb67848a2af9dba9e
DiarrayeDia <diarraye.dia@gmail.com>
2021-06-09 10:12:55 +0200

    added ob_start()

--- controller/errorController.php
+++ controller/errorController.php
@@ -1,5 +1,9 @@
 <?php
 function error404()
 {
+    ob_start();
     require dirname(__DIR__) . '/view/error/error404.php';
+    $content = ob_get_clean();
+
+    require dirname(__DIR__) . '/view/base.php';
 }
